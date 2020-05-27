5ffc2a2da54eb16d715a4e3c3fcf222b2ba0f4db
priyankagarad <garadpriyanka1995@gmail.com>
2020-05-27 19:33:53 +0530

    ADDED:Create Class Police Depatment

---
+++ src/main/java/com/bl/parkinglot/PoliceDepartment.java
@@ -0,0 +1,18 @@
+package com.bl.parkinglot;
+import com.bl.parkinglot.model.Observer;
+public class PoliceDepartment implements Observer {
+    private String vehicleColore;
+    public String getVehicleColore() {
+        return vehicleColore;
+    }
+
+    public void setVehicleColore(String vehicleColore) {
+        this.vehicleColore = vehicleColore;
+    }
+
+    @Override
+    public void update(Object status) {
+        this.setVehicleColore((String) status);
+    }
+}
+
